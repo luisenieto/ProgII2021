3f329dd9f56c27543805a7b5b20a768924ca7539
Luis Nieto <lnieto@herrera.unt.edu.ar>
2021-09-28 14:52:57 -0300

    Unidad XI (contenedores nivel superior)

---
+++ src/gui/Principal1.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author root
+ */
+public class Principal1 {
+    public static void main(String[] args) {
+//        JFrame ventana = new JFrame("Ventana JFrame");
+//        ventana.setSize(300, 400);
+//        ventana.setResizable(false);
+//        ventana.setLocationRelativeTo(null);
+//        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        ventana.setVisible(true);
+
+        VentanaJFrame ventanaJFrame = new VentanaJFrame();
+//        ventana.setLocationRelativeTo(null);
+//        ventana.setVisible(true);
+//        int opcion = JOptionPane.showConfirmDialog(null,
+// 		"¿Desea borrar el espacio especificado?", 
+// 		"Reservas", 
+// 		JOptionPane.YES_NO_OPTION);
+        
+        //System.out.println(opcion);
+        
+        
+//        int opcion = JOptionPane.showOptionDialog(null,
+//            "¿Desea borrar el espacio especificado?", 
+//            "Reservas", JOptionPane.YES_NO_OPTION, 
+//            JOptionPane.QUESTION_MESSAGE, null, 
+//            new Object[] {"Sí", "No"}, "No");      
+
+//        JOptionPane.showMessageDialog(null, 
+//                "No se puede borrar el espacio", 
+//                "Reservas", 
+// 		JOptionPane.ERROR_MESSAGE);
+        
+//        if (opcion == JOptionPane.YES_OPTION)
+//            System.out.println("Se eligió Yes");
+//        else
+//            System.out.println("Se eligió No");
+        
+        VentanaJDialog ventanaJDialog = new VentanaJDialog(ventanaJFrame, false);
+        
+    }    
+}
